21491465e89ed486ac0681343c0af655deee6431
Unknown <hawkest14@gmail.com>
2017-11-06 20:03:20 -0700

    vector3, quatriolion

---
+++ Chicken Chaser/Assets/Scripts/generic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class generic : MonoBehaviour {
+
+	public float posX;
+	public float posY;
+	public float posZ;
+	public float rotX;
+	public float rotY;
+	public float rotZ;
+	public float rotW;
+
+
+
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(posX,posY,posZ);
+		transform.rotation = new Quaternion(rotX,rotY,rotZ,rotW);
+		
+	}
+}
